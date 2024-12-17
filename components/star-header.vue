2399236e79ca6281173cd5633e2c6775ab56b41e
jordan-furr <jordanfurr98@gmail.com>
2024-12-17 10:41:12 +0100

    star laces menu

--- components/star-header.vue
+++ components/star-header.vue
@@ -2,19 +2,25 @@
     <nav>
         <div class="flex flex-row space-between align-center pv3 ph6 header">
             <div class="flex flex-row flex-start menu-bar">
-                <p><a href="/shop" class="menu-item" exact-active-class="active">SHOP</a></p>
+                
+                <!---
                 <p><a href="/" class="menu-item" exact-active-class="active">APPLY</a></p>
+                --->
             </div>
             <div class="text-center margin-a">
                 <a href="/"><img class="logo" src="/img/starlaces.png" alt="star laces llc"></a>
                 
             </div>
             <div class="flex flex-end menu-bar">
-                <p><a href="/" class="menu-item" exact-active-class="active">ABOUT</a></p>
+                
                 <!---
+                <p><a href="/" class="menu-item" exact-active-class="active">ABOUT</a></p>
                 <p><a href="/cart" class="menu-item" exact-active-class="active">CART</a></p>
                 --->
             </div>
+        </div>
+        <div class="shop-button">
+            <p><a href="/shop" class="menu-item" exact-active-class="active">SHOP</a></p>
         </div>
     </nav>
 </template>
